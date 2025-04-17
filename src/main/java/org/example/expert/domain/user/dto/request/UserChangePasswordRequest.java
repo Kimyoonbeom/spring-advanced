47171a38bc1d9ac5922538fea8d7309e54963e5e
Kimyoonbeom <kimybeom@naver.com>
2025-04-17 17:22:09 +0900

    레벨 1-3: Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    @NotBlank
+    @NotBlank(message = "기존 비밀번호를 입력해주세요.")
     private String oldPassword;
-    @NotBlank
+    @NotBlank(message = "새 비밀번호를 입력해주세요.")
+    @Size(min = 8, message = "비밀번호는 8자 이상이여야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*\\d).+$",
+            message = "숫자와 대문자를 포함해야 합니다."
+    )
     private String newPassword;
 }
